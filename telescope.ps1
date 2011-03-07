cbbc24392461fe9a3ae2bb22a38478e91b45165b
ang3lfir3 <ang3lfir3@gmail.com>
2011-03-07 08:14:32 -0800

    adding some pretty output to make it easier to find the right repo

--- telescope.ps1
+++ telescope.ps1
@@ -5,7 +5,19 @@ function search {
 	param($term)
 	$i = 0
 	$j = & .\tools\curl -s0 "http://github.com/api/v2/json/repos/search/$term" | ConvertFrom-JSON  -Type PSObject
-	$j.repositories | Foreach { "" + ++$i + ") " + $_.url}
+	$j.repositories | Foreach {++$i; display_output $_ $i;}
+}
+
+function display_output
+{
+	param([PSObject] $obj, [int] $i)
+	
+	$header = "" + $i + ") " + $obj.url
+	
+	Write-Host $header
+	Write-Host "    "$obj.description
+	Write-Host "     is_a_fork:" $obj.fork " forks:" $obj.forks " watchers:" $obj.watchers
+	Write-Host ""
 }
 
 search $search
