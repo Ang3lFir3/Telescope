91cca37dce1e8fb8b02a570014c56949b3217740
Ang3lFir3 <ang3lfir3@gmail.com>
2011-08-30 18:02:11 -0700

    adding gitignore and updating telescope to use new module.... still some issues with reading the results from the v3 api

--- telescope.ps1
+++ telescope.ps1
@@ -1,15 +1,22 @@
-param($search)
+param($command, $term)
 
-import-module .\modules\JSON\JSON
+import-module .\modules\ConvertFrom-JSON.psm1
 function search {
 	param($term)
 	$i = 0
-	$j = & .\tools\curl -s0 "http://github.com/api/v2/json/repos/search/$term" | ConvertFrom-JSON  -Type PSObject
-	$j.repositories | Foreach {++$i; display_output $_ $i;}
+	$j = & .\tools\curl -s0 "http://github.com/api/v2/json/repos/search/$term" | ConvertFrom-JSON
+	$j.repositories | Foreach {++$i; display_repo_output $_ $i;}
 }
 
-function display_output
-{
+function search_user_repos {
+	param($term)
+	$i = 0
+	$j = & .\tools\curl -k -s0 "https://api.github.com/users/$term/repos"
+	$j = [String]::Join('', $j) | ConvertFrom-JSON
+	$j | Foreach {++$i; display_user_repo_output $_ $i;}
+}
+
+function display_repo_output {
 	param([PSObject] $obj, [int] $i)
 	
 	$header = "" + $i + ") " + $obj.url
@@ -20,6 +27,18 @@ function display_output
 	Write-Host ""
 }
 
-search $search
+function display_user_repo_output {
+	param([PSObject] $obj, [int] $i)
+	
+	#$header = "" + $i + ") " + $obj.name
+	
+	#Write-Host $header
+	Write-Host "    "$obj.ssh_url
+	Write-Host "    "$obj.clone_url
+	Write-Host ""
+}
 
-remove-module JSON
+if($command -eq "repos") { search $term }
+if($command -eq "user_repos") { search_user_repos $term }
+
+remove-module ConvertFrom-JSON
